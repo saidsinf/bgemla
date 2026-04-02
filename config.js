1ed777f620766556508ab3c5ca9e1e11b505f6d4
saidsinf <128310858+saidsinf@users.noreply.github.com>
2026-04-02 21:34:06 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,8 @@
 var SUPABASE_URL = "https://jukvabggadpumtpahsjv.supabase.co";
 var SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp1a3ZhYmdnYWRwdW10cGFoc2p2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUxNDI1MTAsImV4cCI6MjA5MDcxODUxMH0.lBtWTl79l5ZcjY7VcSi_ETlXTBzyjM-mC6fPUrko4Fo";
 
-// Admin password hash — default password: "bgemla2026"
-var ADMIN_HASH = "c9e1a4312c10a8b3a3e6d05bc7e173cfb29854e86bfa51f620f1b61c5e926f3a";
+// Admin password hash for "bgemla2026"
+var ADMIN_HASH = "3508dc750ba5546a4afa3ccd0959603df3a412268fafab469ada4e01a383867e";
 
 var sb = null;
 function initSupabase() {
